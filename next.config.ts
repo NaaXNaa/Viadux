89106b50debcd5633fca53fe8ae63115b0534968
Aruna P <arunacakep@gmail.com>
2025-06-11 23:59:33 +0700

    eslint eror

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    serverActions: true,
+  },
+}
 
+module.exports = nextConfig
 export default nextConfig;
